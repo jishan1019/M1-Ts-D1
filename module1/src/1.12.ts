0a70286dd1868fdcb7642344d46f2a59b01a638b
Jishan Hossain <imranislamjishan200@gmail.com>
2024-05-01 18:52:26 +0600

    practice never unknown,null type

--- module1/src/1.12.ts
+++ module1/src/1.12.ts
@@ -1,2 +1,27 @@
 {
+  const searchName = (value: string) => {
+    if (value) {
+      console.log("searching");
+    } else {
+      console.log("Theere is nothing to search");
+    }
+  };
+
+  searchName("Some");
+
+  const getSpeed = (value: unknown) => {
+    if (typeof value === "number") {
+      console.log(value * 100);
+    } else {
+      console.log("Theere is nothing to search");
+    }
+  };
+
+  getSpeed(20);
+
+  const throwError = (err: string): never => {
+    throw new Error(err);
+  };
+
+  throwError("Something went wrong");
 }
